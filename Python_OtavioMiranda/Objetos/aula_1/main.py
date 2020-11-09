14aac6c90d4865acb57048b1a2187f5c8e3ff235
Everton Silva <57024315+evertonos2312@users.noreply.github.com>
2020-11-09 17:48:08 -0300

    Projeto Bootstrap

---
+++ Python_OtavioMiranda/Objetos/aula_1/main.py
@@ -0,0 +1,10 @@
+from pessoa import Pessoa
+
+p1 = Pessoa('Everton', 28)
+p1.comer('maçã')
+p1.falar('POO')
+p1.parar_comer()
+p1.falar('POO')
+p1.comer('carne')
+p1.parar_falar()
+p1.comer('carne')
